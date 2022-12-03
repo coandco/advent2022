705037d34141b27d7fb6e439fe31a7a92131b0e7
Clint Olson <coandco@gmail.com>
2022-12-02 21:32:39 -0800

    Slightly cleaner day 3

--- advent2022_day03.py
+++ advent2022_day03.py
@@ -12,8 +12,8 @@ MAPPING.update({chr(x): x - 38 for x in range(65, 91)})
 
 if __name__ == '__main__':
     # Get the intersection of the first half of the string and the second half of the string
-    common_elements = [set(x[:len(x)//2]).intersection(set(x[len(x)//2:])).pop() for x in INPUT]
+    common_elements = [(set(x[:len(x)//2]) & set(x[len(x)//2:])).pop() for x in INPUT]
     print(f"Part 1: {sum(MAPPING[x] for x in common_elements)}")
     # Get the intersection of all three strings
-    common_elements = [set(x[0]).intersection(set(x[1])).intersection(set(x[2])).pop() for x in grouper(INPUT, 3)]
+    common_elements = [(set(x[0]) & set(x[1]) & set(x[2])).pop() for x in grouper(INPUT, 3)]
     print(f"Part 2: {sum(MAPPING[x] for x in common_elements)}")
