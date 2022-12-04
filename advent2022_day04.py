b5236c4ac7f092aac9c8cfd7e89ba45242657a91
Clint Olson <coandco@gmail.com>
2022-12-04 07:56:10 -0800

    Switch to using ranges instead of sets for day 4

--- advent2022_day04.py
+++ advent2022_day04.py
@@ -5,14 +5,26 @@ import re
 DIGITS = re.compile(r'\d+')
 
 
-def line_to_sets(line: str) -> Tuple[Set, Set]:
+def line_to_ranges(line: str) -> Tuple[range, range]:
     first_low, first_high, second_low, second_high = (int(x) for x in DIGITS.findall(line))
-    return set(range(first_low, first_high+1)), set(range(second_low, second_high+1))
+    return range(first_low, first_high+1), range(second_low, second_high+1)
 
 
-INPUT = [line_to_sets(x) for x in read_data().splitlines()]
+def ranges_are_subsets(range1: range, range2: range) -> bool:
+    # If the start/end of either range is contained in the other, one's a subset of the other
+    return (range1.start in range2 and range1[-1] in range2) or (range2.start in range1 and range2[-1] in range1)
+
+
+def ranges_overlap(range1: range, range2: range) -> bool:
+    # Try to construct a range with the greater of the two starts and the lesser of the two ends
+    # If they don't overlap, the range will end up empty (and thus evaluate to False)
+    return bool(range(max(range1.start, range2.start), min(range1[-1], range2[-1])+1))
+
+
+# INPUT = [line_to_sets(x) for x in read_data().splitlines()]
+INPUT = [line_to_ranges(x) for x in read_data().splitlines()]
 
 if __name__ == '__main__':
-    print(f"Part one: {len([x for x in INPUT if x[0].issubset(x[1]) or x[1].issubset(x[0])])}")
-    print(f"Part two: {len([x for x in INPUT if x[0].intersection(x[1])])}")
+    print(f"Part one: {len([x for x in INPUT if ranges_are_subsets(*x)])}")
+    print(f"Part two: {len([x for x in INPUT if ranges_overlap(*x)])}")
 
