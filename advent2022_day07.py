e6724ca79082b32775515ddee057090919320625
Clint Olson <coandco@gmail.com>
2022-12-07 10:14:57 -0800

    Refactor day 7 to eliminate the need for a separate class

--- advent2022_day07.py
+++ advent2022_day07.py
@@ -1,41 +1,31 @@
-from typing import Optional, Dict
+from typing import Dict, Tuple, List, Iterable
+from pathlib import Path
 from utils import read_data
 
 
-class Node:
-    def __init__(self, fullname: str, parent: Optional['Node']):
-        self.fullname = fullname
-        self.name = self.fullname.rsplit('/', maxsplit=1)[-1]
-        self.parent = parent
-        self.cached_size = 0
-        self.files: Dict[str, int] = {}
-        self.children: Dict[str, 'Node'] = {}
-
-    def update_cached_size(self, amount_to_add: int):
-        self.cached_size += amount_to_add
-        if self.parent:
-            self.parent.update_cached_size(amount_to_add)
-
-    def parse_ls(self, ls_output: str):
-        for line in ls_output.splitlines()[1:]:
-            tag, name = line.split(" ", maxsplit=1)
-            if tag == "dir":
-                self.children[name] = Node(f"{self.fullname}/{name}", parent=self)
-            else:
-                self.files[name] = int(tag)
-                self.update_cached_size(self.files[name])
-
-    def build_size_dict(self, size_dict: Dict[str, int]):
-        for child in self.children.values():
-            size_dict[child.fullname] = child.cached_size
-            child.build_size_dict(size_dict)
-
-    def __repr__(self):
-        return f"Node({self.fullname}, {self.cached_size})"
+def affected_folders(node: Path) -> Iterable[Path]:
+    while True:
+        yield node
+        if node.parent == node:
+            break
+        node = node.parent
 
 
-def parse_input(full_input: str) -> Node:
-    root_node = current_node = Node('', parent=None)
+def parse_ls(ls_output: str) -> Tuple[List[str], int]:
+    children = []
+    my_size = 0
+    for line in ls_output.splitlines()[1:]:
+        tag, name = line.split(" ", maxsplit=1)
+        if tag == "dir":
+            children.append(name)
+        else:
+            my_size += int(tag)
+    return children, my_size
+
+
+def parse_input(full_input: str) -> Dict[Path, int]:
+    sizes = {Path("/"): 0}
+    current_node = Path("/")
     commands = full_input.split("$ ")
     # When you split on $ and the file starts with it, you have a blank line to start with
     for command in commands[1:]:
@@ -44,22 +34,23 @@ def parse_input(full_input: str) -> Node:
             if name == "..":
                 current_node = current_node.parent
             elif name == "/":
-                current_node = root_node
+                current_node = Path("/")
             else:
-                current_node = current_node.children[name]
+                current_node = current_node / name
         elif command.startswith("ls"):
-            current_node.parse_ls(command)
+            children, size = parse_ls(command)
+            sizes |= {current_node/x: 0 for x in children}
+            for node in affected_folders(current_node):
+                sizes[node] += size
         else:
             raise Exception(f"Unknown command {command}")
-    return root_node
+    return sizes
 
 
 if __name__ == '__main__':
-    root_node = parse_input(read_data())
-    size_dict = {"/": root_node.cached_size}
-    root_node.build_size_dict(size_dict)
+    size_dict = parse_input(read_data())
     print(f"Part one: {sum(x for x in size_dict.values() if x < 100000)}")
-    free_space = 70000000 - root_node.cached_size
+    free_space = 70000000 - size_dict[Path("/")]
     space_needed = 30000000
     threshold_to_free = space_needed - free_space
     print(f"Part two: {min(x for x in size_dict.values() if x > threshold_to_free)}")
