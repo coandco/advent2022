ae9191c87b5d85b6393bff7673f948a987d742d2
Clint Olson <coandco@gmail.com>
2022-12-09 07:04:17 -0800

    Clean up day 9

--- advent2022_day09.py
+++ advent2022_day09.py
@@ -1,9 +1,5 @@
 from utils import read_data
-from typing import NamedTuple
-
-
-def sign(x: int) -> int:
-    return -1 if x < 0 else 1
+from typing import NamedTuple, List
 
 
 class Coord(NamedTuple):
@@ -16,14 +12,25 @@ class Coord(NamedTuple):
     def __sub__(self, other: 'Coord') -> 'Coord':
         return Coord(x=self.x-other.x, y=self.y-other.y)
 
+    def normalized(self):
+        return Coord(
+            x=0 if self.x == 0 else self.x // abs(self.x),
+            y=0 if self.y == 0 else self.y // abs(self.y)
+        )
+
+    def in_range(self) -> bool:
+        return max(abs(self.x), abs(self.y)) <= 1
+
     def catch_up(self, head: 'Coord') -> 'Coord':
         diff = head - self
-        if max(abs(diff.x), abs(diff.y)) <= 1:
-            return self
-        elif min(abs(diff.x), abs(diff.y)) == 0 or diff.x == diff.y:
-            return self + Coord(x=diff.x//2, y=diff.y//2)
-        else:
-            return self + Coord(x=sign(diff.x), y=sign(diff.y))
+        return self if diff.in_range() else self + diff.normalized()
+
+
+def move_knots(knots: List[Coord], direction: str) -> List[Coord]:
+    new_knots = [knots[0] + DIRECTIONS[direction]]
+    for knot in knots[1:]:
+        new_knots.append(knot.catch_up(new_knots[-1]))
+    return new_knots
 
 
 DIRECTIONS = {
@@ -36,23 +43,20 @@ DIRECTIONS = {
 INPUT = read_data().splitlines()
 
 if __name__ == '__main__':
-    head = tail = Coord(0, 0)
-    tail_locs = {tail}
+    knots = [Coord(0, 0)] * 2
+    tail_locs = set()
     for line in INPUT:
         direction, amount = line.split(" ")
         for _ in range(int(amount)):
-            head = head + DIRECTIONS[direction]
-            tail = tail.catch_up(head)
-            tail_locs.add(tail)
+            knots = move_knots(knots, direction)
+            tail_locs.add(knots[-1])
     print(f"Part one: {len(tail_locs)}")
     knots = [Coord(0, 0)] * 10
     tail_locs = {knots[-1]}
     for line in INPUT:
         direction, amount = line.split(" ")
         for _ in range(int(amount)):
-            knots[0] = knots[0] + DIRECTIONS[direction]
-            for i in range(1, 10):
-                knots[i] = knots[i].catch_up(knots[i-1])
+            knots = move_knots(knots, direction)
             tail_locs.add(knots[-1])
     print(f"Part two: {len(tail_locs)}")
 
