c93ebf4d198a07cebb260f5ac04e86e27bf02644
Clint Olson <coandco@gmail.com>
2022-12-10 10:13:57 -0800

    Clean up day 10

--- advent2022_day10.py
+++ advent2022_day10.py
@@ -1,62 +1,36 @@
-from typing import Tuple, Optional, Union
+from typing import Tuple
 from utils import read_data
 
 
-
-PART_1_TRIGGERS = [20, 60, 100, 140, 180, 220]
-
-
-def sprite_visible(cycle: int, x: int):
-    return ((cycle-1) % 40) in (x-1, x, x+1)
-
-
-def execute_instruction(cycle: int, x: int, instruction: str, part_two=False) -> Tuple[int, int, Optional[int], str]:
-    triggered_signal_strength = None
-    output = "#" if sprite_visible(cycle, x) else "."
-    if cycle in PART_1_TRIGGERS:
-        triggered_signal_strength = cycle * x
-
-    if instruction.startswith("noop"):
-        return cycle + 1, x, triggered_signal_strength, output
-    elif instruction.startswith("addx"):
-        arg = int(instruction.split(" ")[1])
-        cycle += 1
-        output += "#" if sprite_visible(cycle, x) else "."
-        if cycle in PART_1_TRIGGERS and not part_two:
-            triggered_signal_strength = cycle * x
-        return cycle + 1, x + arg, triggered_signal_strength, output
-    else:
-        raise Exception(f"Unknown instruction {instruction}!")
-
-
 # Taken from https://stackoverflow.com/a/2657733
-def insert_newlines(string, every=64):
+def insert_newlines(string, every=40):
     return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
 
 
 # Taken from advent-of-code-ocr
 ALPHABET_6 = {
-    ".##.\n#..#\n#..#\n####\n#..#\n#..#": "A",
-    "###.\n#..#\n###.\n#..#\n#..#\n###.": "B",
-    ".##.\n#..#\n#...\n#...\n#..#\n.##.": "C",
-    "####\n#...\n###.\n#...\n#...\n####": "E",
-    "####\n#...\n###.\n#...\n#...\n#...": "F",
-    ".##.\n#..#\n#...\n#.##\n#..#\n.###": "G",
-    "#..#\n#..#\n####\n#..#\n#..#\n#..#": "H",
-    ".###\n..#.\n..#.\n..#.\n..#.\n.###": "I",
-    "..##\n...#\n...#\n...#\n#..#\n.##.": "J",
-    "#..#\n#.#.\n##..\n#.#.\n#.#.\n#..#": "K",
-    "#...\n#...\n#...\n#...\n#...\n####": "L",
-    ".##.\n#..#\n#..#\n#..#\n#..#\n.##.": "O",
-    "###.\n#..#\n#..#\n###.\n#...\n#...": "P",
-    "###.\n#..#\n#..#\n###.\n#.#.\n#..#": "R",
-    ".###\n#...\n#...\n.##.\n...#\n###.": "S",
-    "#..#\n#..#\n#..#\n#..#\n#..#\n.##.": "U",
-    "#...\n#...\n.#.#\n..#.\n..#.\n..#.": "Y",
-    "####\n...#\n..#.\n.#..\n#...\n####": "Z",
+    " ██ \n█  █\n█  █\n████\n█  █\n█  █": "A",
+    "███ \n█  █\n███ \n█  █\n█  █\n███ ": "B",
+    " ██ \n█  █\n█   \n█   \n█  █\n ██ ": "C",
+    "████\n█   \n███ \n█   \n█   \n████": "E",
+    "████\n█   \n███ \n█   \n█   \n█   ": "F",
+    " ██ \n█  █\n█   \n█ ██\n█  █\n ███": "G",
+    "█  █\n█  █\n████\n█  █\n█  █\n█  █": "H",
+    " ███\n  █ \n  █ \n  █ \n  █ \n ███": "I",
+    "  ██\n   █\n   █\n   █\n█  █\n ██ ": "J",
+    "█  █\n█ █ \n██  \n█ █ \n█ █ \n█  █": "K",
+    "█   \n█   \n█   \n█   \n█   \n████": "L",
+    " ██ \n█  █\n█  █\n█  █\n█  █\n ██ ": "O",
+    "███ \n█  █\n█  █\n███ \n█   \n█   ": "P",
+    "███ \n█  █\n█  █\n███ \n█ █ \n█  █": "R",
+    " ███\n█   \n█   \n ██ \n   █\n███ ": "S",
+    "█  █\n█  █\n█  █\n█  █\n█  █\n ██ ": "U",
+    "█   \n█   \n █ █\n  █ \n  █ \n  █ ": "Y",
+    "████\n   █\n  █ \n █  \n█   \n████": "Z",
 }
 
 
+# Taken from advent-of-code-ocr
 def ocr(input: str) -> str:
     lines = input.splitlines()
     indices = [slice(start, start + 4) for start in range(0, len(lines[0]), 5)]
@@ -67,14 +41,28 @@ def ocr(input: str) -> str:
     return "".join(result)
 
 
+def sprite_visible(cycle: int, x: int):
+    return (cycle % 40) in (x-1, x, x+1)
+
+
+def execute_instruction(x: int, instruction: str, part_two=False) -> Tuple[int, int]:
+    if instruction.startswith("noop"):
+        return x, 1
+    elif instruction.startswith("addx"):
+        arg = int(instruction.split(" ")[1])
+        return x + arg, 2
+    else:
+        raise Exception(f"Unknown instruction {instruction}!")
+
+
 if __name__ == '__main__':
-    cycle = x = 1
-    sampled_values = []
-    output_str = ''
+    # Initialize the 0th item to the starting value, since cycle starts on 1
+    x_values = [1]
+    x = 1
     for line in read_data().splitlines():
-        cycle, x, sampled_value, chars_output = execute_instruction(cycle, x, line)
-        if sampled_value is not None:
-            sampled_values.append(sampled_value)
-        output_str += chars_output
-    print(f"Part one: {sum(sampled_values)}")
-    print(f"Part two: {ocr(insert_newlines(output_str, 40))}")
+        new_x, cycles_taken = execute_instruction(x, line)
+        x_values += [x] * cycles_taken
+        x = new_x
+    print(f"Part one: {sum(x_values[v]*v for v in range(20, 221, 40))}")
+    output_str = insert_newlines(''.join("█" if sprite_visible(i, x) else " " for i, x in enumerate(x_values[1:])))
+    print(f"Part two: {ocr(output_str)}")
