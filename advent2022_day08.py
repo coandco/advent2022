2960296e8bb733b68602d9e48cc5d738aae4dab7
Clint Olson <coandco@gmail.com>
2022-12-12 12:21:22 -0800

    Clean up day 8 with util-class Coord

--- advent2022_day08.py
+++ advent2022_day08.py
@@ -1,17 +1,6 @@
-from utils import read_data
-from typing import NamedTuple, Dict
+from utils import read_data, Coord, CARDINAL_NEIGHBORS_2D
+from typing import Dict
 from math import prod
-
-
-class Coord(NamedTuple):
-    x: int
-    y: int
-
-    def __add__(self, other: 'Coord'):
-        return Coord(x=self.x+other.x, y=self.y+other.y)
-
-
-DIRECTIONS = (Coord(1, 0), Coord(0, 1), Coord(-1, 0), Coord(0, -1))
 
 
 class Forest:
@@ -29,7 +18,7 @@ class Forest:
         return all(self.grid[c] < tree_height for c in self.coords_to_edge(coord, step))
 
     def tree_is_visible(self, coord: Coord) -> bool:
-        return any(self.tree_is_visible_from_direction(coord, step) for step in DIRECTIONS)
+        return any(self.tree_is_visible_from_direction(coord, step) for step in CARDINAL_NEIGHBORS_2D)
 
     def count_visible_trees(self) -> int:
         return sum(self.tree_is_visible(x) for x in self.grid)
@@ -43,7 +32,7 @@ class Forest:
         return trees_visible
 
     def viewing_score(self, coord: Coord):
-        return prod(self.viewing_distance_in_direction(coord, x) for x in DIRECTIONS)
+        return prod(self.viewing_distance_in_direction(coord, x) for x in CARDINAL_NEIGHBORS_2D)
 
     def best_viewing_score(self):
         return max(self.viewing_score(x) for x in self.grid)
