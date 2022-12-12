5d560d50abf6fbdf454d870ed625e55616a2ec9c
Clint Olson <coandco@gmail.com>
2022-12-12 12:18:46 -0800

    Move Coord class to utils and clean up day 12

--- advent2022_day12.py
+++ advent2022_day12.py
@@ -1,17 +1,6 @@
 from collections import deque, defaultdict
-from typing import NamedTuple, Tuple, Deque, DefaultDict, Optional, Iterable, Set, List
-from utils import read_data
-
-
-class Coord(NamedTuple):
-    x: int
-    y: int
-
-    def __add__(self, other: "Coord") -> "Coord":
-        return Coord(x=self.x + other.x, y=self.y + other.y)
-
-
-DIRECTIONS = (Coord(0, 1), Coord(1, 0), Coord(0, -1), Coord(-1, 0))
+from typing import Tuple, Deque, DefaultDict, Optional, Iterable, List
+from utils import read_data, Coord
 
 
 class Heightmap:
@@ -29,24 +18,12 @@ class Heightmap:
                     self.points[self.end] = ord("z")
                 else:
                     self.points[Coord(x=x, y=y)] = ord(char)
-        self.max_x = max(x.x for x in self.points)
-        self.max_y = max(x.y for x in self.points)
         self.valid_dirs = {x: self.valid_directions(x) for x in self.points}
-        self.reversed_valid_dirs: DefaultDict[Coord, Set[Coord]] = defaultdict(set)
-        for k, v in self.valid_dirs.items():
-            for point in v:
-                self.reversed_valid_dirs[point].add(k)
-        self.reversed_heatmap = self.generate_heatmap(self.end)
-
-    def in_bounds(self, point: Coord):
-        return (0 <= point.x <= self.max_x) and (0 <= point.y <= self.max_y)
+        self.heatmap = self.generate_heatmap(self.end)
 
     def valid_directions(self, point: Coord) -> Tuple[Coord]:
-        return tuple(
-            new_loc
-            for x in DIRECTIONS
-            if self.in_bounds(new_loc := point + x) and self.points[new_loc] <= self.points[point] + 1
-        )
+        neighbors = (x for x in point.cardinal_neighbors() if x in self.points)
+        return tuple(x for x in neighbors if self.points[x] >= (self.points[point] - 1))
 
     def generate_heatmap(self, start_coord: Coord) -> DefaultDict[Coord, int]:
         heatmap: DefaultDict[Coord, int] = defaultdict(lambda: 99999)
@@ -55,13 +32,8 @@ class Heightmap:
             loc, score = to_evaluate.popleft()
             if score < heatmap[loc]:
                 heatmap[loc] = score
-                to_evaluate.extend((x, score + 1) for x in self.reversed_valid_dirs[loc])
+                to_evaluate.extend((x, score + 1) for x in self.valid_dirs[loc])
         return heatmap
-
-    def get_shortest_paths(self, start_coords: Optional[Iterable] = None) -> List[int]:
-        if not start_coords:
-            start_coords = [self.start]
-        return [self.reversed_heatmap[x] for x in start_coords]
 
     def all_of_height(self, height: str) -> Iterable:
         return (k for k, v in self.points.items() if v == ord(height))
@@ -69,8 +41,8 @@ class Heightmap:
 
 def main(input_str: str):
     hmap = Heightmap(input_str)
-    print(f"Part one: {hmap.get_shortest_paths()[0]}")
-    print(f"Part two: {min(hmap.get_shortest_paths(hmap.all_of_height('a')))}")
+    print(f"Part one: {hmap.heatmap[hmap.start]}")
+    print(f"Part two: {min(hmap.heatmap[x] for x in hmap.all_of_height('a'))}")
 
 
 if __name__ == "__main__":
